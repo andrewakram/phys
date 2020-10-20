c515bc1e31491e4a075ed292ed140b00c8f57727
unknown <andro_akram@2grand.net>
2020-10-20 19:19:39 +0200

    20_10_2020

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -15,22 +15,22 @@
                     <div class="menu-extras topbar-custom">
 
                         <!-- Search input -->
-                        <div class="search-wrap" id="search-wrap">
-                            <div class="search-bar">
-                                <input class="search-input" type="search" placeholder="Search" />
-                                <a href="#" class="close-search toggle-search" data-target="#search-wrap">
-                                    <i class="mdi mdi-close-circle"></i>
-                                </a>
-                            </div>
-                        </div>
+{{--                        <div class="search-wrap" id="search-wrap">--}}
+{{--                            <div class="search-bar">--}}
+{{--                                <input class="search-input" type="search" placeholder="Search" />--}}
+{{--                                <a href="#" class="close-search toggle-search" data-target="#search-wrap">--}}
+{{--                                    <i class="mdi mdi-close-circle"></i>--}}
+{{--                                </a>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
 
                         <ul class="list-inline float-right mb-0">
                             <!-- Search -->
-                            <li class="list-inline-item dropdown notification-list">
-                                <a class="nav-link waves-effect toggle-search" href="#"  data-target="#search-wrap">
-                                    <i class="mdi mdi-magnify noti-icon"></i>
-                                </a>
-                            </li>
+{{--                            <li class="list-inline-item dropdown notification-list">--}}
+{{--                                <a class="nav-link waves-effect toggle-search" href="#"  data-target="#search-wrap">--}}
+{{--                                    <i class="mdi mdi-magnify noti-icon"></i>--}}
+{{--                                </a>--}}
+{{--                            </li>--}}
                             <!-- Fullscreen -->
                             <li class="list-inline-item dropdown notification-list hide-phone">
                                 <a class="nav-link waves-effect" href="#" id="btn-fullscreen">
@@ -38,57 +38,57 @@
                                 </a>
                             </li>
                             <!-- language-->
-                            <li class="list-inline-item dropdown notification-list hide-phone">
-                                <a class="nav-link dropdown-toggle arrow-none waves-effect" data-toggle="dropdown" href="#" role="button"
-                                   aria-haspopup="false" aria-expanded="false">
-                                    English <img src="{{ URL::asset('assets/images/flags/us_flag.jpg') }}" class="ml-2" height="16" alt=""/>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-right language-switch">
-                                    <a class="dropdown-item" href="#"><img src="{{ URL::asset('assets/images/flags/germany_flag.jpg') }}" alt="" height="16"/><span> German </span></a>
-                                    <a class="dropdown-item" href="#"><img src="{{ URL::asset('assets/images/flags/italy_flag.jpg') }}" alt="" height="16"/><span> Italian </span></a>
-                                    <a class="dropdown-item" href="#"><img src="{{ URL::asset('assets/images/flags/french_flag.jpg') }}" alt="" height="16"/><span> French </span></a>
-                                    <a class="dropdown-item" href="#"><img src="{{ URL::asset('assets/images/flags/spain_flag.jpg') }}" alt="" height="16"/><span> Spanish </span></a>
-                                    <a class="dropdown-item" href="#"><img src="{{ URL::asset('assets/images/flags/russia_flag.jpg') }}" alt="" height="16"/><span> Russian </span></a>
-                                </div>
-                            </li>
+{{--                            <li class="list-inline-item dropdown notification-list hide-phone">--}}
+{{--                                <a class="nav-link dropdown-toggle arrow-none waves-effect" data-toggle="dropdown" href="#" role="button"--}}
+{{--                                   aria-haspopup="false" aria-expanded="false">--}}
+{{--                                    English <img src="{{ URL::asset('assets/images/flags/us_flag.jpg') }}" class="ml-2" height="16" alt=""/>--}}
+{{--                                </a>--}}
+{{--                                <div class="dropdown-menu dropdown-menu-right language-switch">--}}
+{{--                                    <a class="dropdown-item" href="#"><img src="{{ URL::asset('assets/images/flags/germany_flag.jpg') }}" alt="" height="16"/><span> German </span></a>--}}
+{{--                                    <a class="dropdown-item" href="#"><img src="{{ URL::asset('assets/images/flags/italy_flag.jpg') }}" alt="" height="16"/><span> Italian </span></a>--}}
+{{--                                    <a class="dropdown-item" href="#"><img src="{{ URL::asset('assets/images/flags/french_flag.jpg') }}" alt="" height="16"/><span> French </span></a>--}}
+{{--                                    <a class="dropdown-item" href="#"><img src="{{ URL::asset('assets/images/flags/spain_flag.jpg') }}" alt="" height="16"/><span> Spanish </span></a>--}}
+{{--                                    <a class="dropdown-item" href="#"><img src="{{ URL::asset('assets/images/flags/russia_flag.jpg') }}" alt="" height="16"/><span> Russian </span></a>--}}
+{{--                                </div>--}}
+{{--                            </li>--}}
                             <!-- notification-->
-                            <li class="list-inline-item dropdown notification-list">
-                                <a class="nav-link dropdown-toggle arrow-none waves-effect" data-toggle="dropdown" href="#" role="button"
-                                   aria-haspopup="false" aria-expanded="false">
-                                    <i class="ion-ios7-bell noti-icon"></i>
-                                    <span class="badge badge-danger noti-icon-badge">3</span>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-right dropdown-arrow dropdown-menu-lg">
-                                    <!-- item-->
-                                    <div class="dropdown-item noti-title">
-                                        <h5>Notification (3)</h5>
-                                    </div>
+{{--                            <li class="list-inline-item dropdown notification-list">--}}
+{{--                                <a class="nav-link dropdown-toggle arrow-none waves-effect" data-toggle="dropdown" href="#" role="button"--}}
+{{--                                   aria-haspopup="false" aria-expanded="false">--}}
+{{--                                    <i class="ion-ios7-bell noti-icon"></i>--}}
+{{--                                    <span class="badge badge-danger noti-icon-badge">3</span>--}}
+{{--                                </a>--}}
+{{--                                <div class="dropdown-menu dropdown-menu-right dropdown-arrow dropdown-menu-lg">--}}
+{{--                                    <!-- item-->--}}
+{{--                                    <div class="dropdown-item noti-title">--}}
+{{--                                        <h5>Notification (3)</h5>--}}
+{{--                                    </div>--}}
 
-                                    <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item active">
-                                        <div class="notify-icon bg-success"><i class="mdi mdi-cart-outline"></i></div>
-                                        <p class="notify-details"><b>Your order is placed</b><small class="text-muted">Dummy text of the printing and typesetting industry.</small></p>
-                                    </a>
+{{--                                    <!-- item-->--}}
+{{--                                    <a href="javascript:void(0);" class="dropdown-item notify-item active">--}}
+{{--                                        <div class="notify-icon bg-success"><i class="mdi mdi-cart-outline"></i></div>--}}
+{{--                                        <p class="notify-details"><b>Your order is placed</b><small class="text-muted">Dummy text of the printing and typesetting industry.</small></p>--}}
+{{--                                    </a>--}}
 
-                                    <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
-                                        <div class="notify-icon bg-warning"><i class="mdi mdi-message"></i></div>
-                                        <p class="notify-details"><b>New Message received</b><small class="text-muted">You have 87 unread messages</small></p>
-                                    </a>
+{{--                                    <!-- item-->--}}
+{{--                                    <a href="javascript:void(0);" class="dropdown-item notify-item">--}}
+{{--                                        <div class="notify-icon bg-warning"><i class="mdi mdi-message"></i></div>--}}
+{{--                                        <p class="notify-details"><b>New Message received</b><small class="text-muted">You have 87 unread messages</small></p>--}}
+{{--                                    </a>--}}
 
-                                    <!-- item-->
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
-                                        <div class="notify-icon bg-info"><i class="mdi mdi-martini"></i></div>
-                                        <p class="notify-details"><b>Your item is shipped</b><small class="text-muted">It is a long established fact that a reader will</small></p>
-                                    </a>
+{{--                                    <!-- item-->--}}
+{{--                                    <a href="javascript:void(0);" class="dropdown-item notify-item">--}}
+{{--                                        <div class="notify-icon bg-info"><i class="mdi mdi-martini"></i></div>--}}
+{{--                                        <p class="notify-details"><b>Your item is shipped</b><small class="text-muted">It is a long established fact that a reader will</small></p>--}}
+{{--                                    </a>--}}
 
-                                    <!-- All-->
-                                    <a href="javascript:void(0);" class="dropdown-item notify-item">
-                                        View All
-                                    </a>
+{{--                                    <!-- All-->--}}
+{{--                                    <a href="javascript:void(0);" class="dropdown-item notify-item">--}}
+{{--                                        View All--}}
+{{--                                    </a>--}}
 
-                                </div>
-                            </li>
+{{--                                </div>--}}
+{{--                            </li>--}}
                             <!-- User-->
                             <li class="list-inline-item dropdown notification-list">
                                 <a class="nav-link dropdown-toggle arrow-none waves-effect nav-user" data-toggle="dropdown" href="#" role="button"
